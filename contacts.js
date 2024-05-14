2a6b7f9918244483ea3a4a0b7af39699b4383321
Maxim <kmaxusk@gmail.com>
2024-05-14 22:38:19 +0300

    step2 start

---
+++ contacts.js
@@ -0,0 +1,21 @@
+import path from "path";
+import fs from "fs/promises";
+
+const contactsPath = path.resolve("db", "contacts.json");
+
+export async function listContacts() {
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
+}
+
+async function getContactById(contactId) {
+  // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
+}
+
+async function removeContact(contactId) {
+  // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
+}
+
+async function addContact(name, email, phone) {
+  // ...твій код. Повертає об'єкт доданого контакту (з id).
+}
--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import { program } from "commander";
+import { listContacts } from "./contacts.js";
+
 program
   .option("-a, --action <type>", "choose action")
   .option("-i, --id <type>", "user id")
@@ -10,28 +12,28 @@ program.parse();
 
 const options = program.opts();
 
-// TODO: рефакторити
-async function invokeAction({ action, id, name, email, phone }) {
-  switch (action) {
-    case "list":
-      // ...
-      break;
+// // TODO: рефакторити
+// async function invokeAction({ action, id, name, email, phone }) {
+//   switch (action) {
+//     case "list":
+//       // ...
+//       break;
 
-    case "get":
-      // ... id
-      break;
+//     case "get":
+//       // ... id
+//       break;
 
-    case "add":
-      // ... name email phone
-      break;
+//     case "add":
+//       // ... name email phone
+//       break;
 
-    case "remove":
-      // ... id
-      break;
+//     case "remove":
+//       // ... id
+//       break;
 
-    default:
-      console.warn("\x1B[31m Unknown action type!");
-  }
-}
+//     default:
+//       console.warn("\x1B[31m Unknown action type!");
+//   }
+// }
 
-invokeAction(options);
+// invokeAction(options);
